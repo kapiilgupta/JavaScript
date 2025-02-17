7f3c19444b1d7a1bf6e20c52200d62e881c080d1
kapiilgupta <kapil.gupta2024@lpu.in>
2025-02-17 16:14:27 +0000

    Operations

--- 01_Basics/03_conversionsOperation.js
+++ 01_Basics/03_conversionsOperation.js
@@ -26,12 +26,12 @@ let ans = Boolean(loggedIn);
 console.log(ans); //output = true
 
 
-//input -> output
-//1 = true
-//1345 = true
-//0 = false
-//"" = false //empty string
-//"sf" = true //non-empty string
+// //input -> output
+// //1 = true
+// //1345 = true
+// //0 = false
+// //"" = false //empty string
+// //"sf" = true //non-empty string
 
 
 //conversion to string
@@ -41,6 +41,36 @@ console.log(bool); //output = true
 let ansBool = String(bool);
 //print and check what it shows
 console.log(ansBool); //output = true
-console.log(typeof ansBool); //output = string
+console.log(typeof ansBool, "\n\n"); //output = string
 
+
+// ***************OPERATORS*****************
+console.log("Operations\n");
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+
+let str1 = "Hello ";
+let str2 = "World";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12 
+console.log(1 + 2 + "2"); //32
+console.log("1" + 2 + 2); //122
+
+console.log(+true); //true is 1 in boolean so it givevs you 1 as output
+console.log(+""); // empty "" inverted comma is false in boolean sso it gives you output as 0. 
+
+let num1, num2 , num3;
+num1 = num2 = num3 = 2+2; //generally we don't do this, this is a bad practice
+console.log(num1);
+console.log(num2);
+console.log(num3);
 
